0ad1c15545d5f7e28b8805290664185965c97197
Gaetan Therou <gaetan.therou@epitech.eu>
2018-01-13 16:13:21 -0500

    MAJ all

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -1,80 +1,70 @@
 #include "Logger.hpp"
 
-Logger Logger::_instance = Logger();
+namespace Network {
+    Logger Logger::mInstance = Logger();
 
-Logger::Logger()
-{
-	Init();
-}
-
-Logger::Logger(const Logger & log)
-{
-	Init();
-}
-
-
-Logger & Logger::GetInstance()
-{
-    return _instance;
-}
-
-void Logger::Init()
-{
-    std::string filename("log-");
-    time_t now = time(0);
-	struct tm timeinfo;
-    localtime_s(&timeinfo, &now);
-    filename.append(std::to_string(timeinfo.tm_mday)).append("-")
-        .append(std::to_string(1 + timeinfo.tm_mon)).append("-")
-        .append(std::to_string(1900 + timeinfo.tm_year)).append(".log");
-    std::cout << "Initializing logger in " << filename << "... ";
-    _fstream.open(filename, std::fstream::out | std::fstream::app);
-    if (!_fstream.is_open())
-    {
-        std::cout << "error!" << std::endl;
-        std::cerr << "Error while opening file" << std::endl;
+    Logger::Logger() {
+        Init();
     }
-    else
-        std::cout << "done!" << std::endl;
-}
 
-void Logger::Quit()
-{
-    if (_fstream.is_open())
-    {
-        std::cout << "Closing file... ";
-        _fstream.close();
-        if (_fstream.is_open())
+    Logger::Logger(const Logger &log) {
+        Init();
+    }
+
+
+    Logger &Logger::GetInstance() {
+        return mInstance;
+    }
+
+    void Logger::Init() {
+        std::string filename("log-");
+        time_t now = time(0);
+        struct tm timeinfo;
+#ifdef TARGET_OS_WIN32
+        localtime_s(&timeinfo, &now);
+#else
+        localtime_r(&now, &timeinfo);
+#endif /*__linux*/
+        filename.append(std::to_string(timeinfo.tm_mday)).append("-")
+                .append(std::to_string(1 + timeinfo.tm_mon)).append("-")
+                .append(std::to_string(1900 + timeinfo.tm_year)).append(".log");
+        std::cout << "Initializing logger in " << filename << "... ";
+        mFstream.open(filename, std::fstream::out | std::fstream::app);
+        if (!mFstream.is_open()) {
             std::cout << "error!" << std::endl;
-        else
+            std::cerr << "Error while opening file" << std::endl;
+        } else
             std::cout << "done!" << std::endl;
     }
-}
 
-void Logger::LogLine(std::string const & msg)
-{
-    if (_fstream.is_open())
-    {
-        _fstream << msg << std::endl;
-        _fstream.flush();
-        std::cout << msg << std::endl;
+    void Logger::Quit() {
+        if (mFstream.is_open()) {
+            std::cout << "Closing file... ";
+            mFstream.close();
+            if (mFstream.is_open())
+                std::cout << "error!" << std::endl;
+            else
+                std::cout << "done!" << std::endl;
+        }
     }
-    else
-    {
-        std::cerr << "Error while loging mesage: \"" << msg << "\"" << std::endl;
-    }
-}
 
-void Logger::LogMsg(std::string const & msg)
-{
-    if (_fstream.is_open())
-    {
-        _fstream << msg;
-        _fstream.flush();
-        std::cout << msg;
+    void Logger::LogLine(const std::string &msg) {
+        if (mFstream.is_open()) {
+            mFstream << msg << std::endl;
+            mFstream.flush();
+            std::cout << msg << std::endl;
+        } else {
+            std::cerr << "Error while loging mesage: \"" << msg << "\"" << std::endl;
+        }
     }
-    else
-    {
-        std::cerr << "Error while loging mesage: " << msg << std::endl;
+
+    void Logger::LogMsg(const std::string &msg) {
+        if (mFstream.is_open()) {
+            mFstream << msg;
+            mFstream.flush();
+            std::cout << msg;
+        } else {
+            std::cerr << "Error while loging mesage: " << msg << std::endl;
+        }
     }
 }
--- Logger/Logger.hpp
+++ Logger/Logger.hpp
@@ -1,52 +1,56 @@
 #ifndef LOGGER_HPP_
 #define LOGGER_HPP_
 
+#include <time.h>
 #include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
 
-class Logger
-{
-public:
-	~Logger() {};
+namespace Network {
+    class Logger {
+    public:
+        ~Logger() {};
 
-public:
-	/*
-	*	Get the unique instance of "Logger"
-	*/
-	static Logger	&GetInstance();
+    public:
+        /*
+        *	Get the unique instance of "Logger"
+        */
+        static Logger &GetInstance();
 
-	/*
-	*	close log file .
-	*/
-    void			Quit();
+        /*
+        *	close log file.
+        */
+        void Quit();
 
-	/*
-	*	write line, in log file and in std out.
-	*
-	*	@param "msg" -> is a message at write
-	*/
-    void			LogLine(std::string const &msg);
+        /*
+        *	write line, in log file and in std out.
+        *
+        *	@param "msg" -> is a message at write
+        */
+        void LogLine(const std::string &msg);
 
-	/*
-	*	write msg, in log file and in std out.
-	*
-	*	@param "msg" -> print msg on the end of file
-	*/
-    void			LogMsg(std::string const &msg);
+        /*
+        *	write msg, in log file and in std out.
+        *
+        *	@param "msg" -> print msg on the end of file
+        */
+        void LogMsg(const std::string &msg);
 
-	/*
-	*	Initialize, create, or append log file.
-	*/
-	void			Init();
+        /*
+        *	Initialize, create, or append log file.
+        */
+        void Init();
 
-private:
-	Logger(const Logger &log);
-	Logger();
+    private:
+        Logger(const Logger &log);
 
-    static Logger	_instance;
-    std::fstream	_fstream;
-};
+        Logger();
+
+    public:
+        static Logger mInstance;
+        std::fstream mFstream;
+    };
+}
 
 #endif
